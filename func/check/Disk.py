032159e1716eb79e19a7cd09b1224987bfaa6d77
za5321 <za5321@naver.com>
2022-10-24 17:40:00 +0900

    Changed disk_usage from used to free

--- func/check/Disk.py
+++ func/check/Disk.py
@@ -1,13 +1,14 @@
 class Disk:
     def __init__(self, letter: str):
-        self.letter = letter
+        self.letter = letter + ":"
 
     def get_disk(self) -> str:
         import shutil
+
         try:
             usage = shutil.disk_usage(self.letter)
         except FileNotFoundError:
             return "NODATA"
         else:
-            percent = int(100 - (usage.free / usage.total * 100))
+            percent = int(usage.used / usage.total * 100)
             return f"{percent}%"
